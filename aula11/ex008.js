1455a6185c472c859d16b4c6d71d8095a4d4b4ee
dani-furtado <danicfurtado@outlook.com>
2024-01-15 17:44:13 -0300

    Exercícios da aula

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+// console.log('O console funcionou perfeitamente')
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
