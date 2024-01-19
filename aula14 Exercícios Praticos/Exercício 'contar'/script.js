8bb6e0f14951bdee33b4d1164ac4e81b48662945
dani-furtado <danicfurtado@outlook.com>
2024-01-19 13:45:40 -0300

    Adicionando exercícios práticos

---
+++ aula14 Exercícios Praticos/Exercício 'contar'/script.js
@@ -0,0 +1,28 @@
+function contar (){
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p<=0){
+            window.alert('Passo inválido! Considerando passo = 1')
+            p = 1
+        }
+        if (i < f) {
+        for (var c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1f449}`
+        }
+        } else { 
+            for(var c = i; c>= f; c -=p)
+                res.innerHTML += ` ${c} \u{1f449}`
+        }
+    res.innerHTML += '\u{1f3c1}'
+    }
+}
